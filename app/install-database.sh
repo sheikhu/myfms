98eae14a3b90c62d3d111d35a7fad066cc4babeb
Dale-Kurt Murray <dalekurt.murray@gmail.com>
2015-07-31 12:47:12 -0400

    Update

--- app/install-database.sh
+++ app/install-database.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if psql -lqt -h db -U fms | cut -d \| -f 1 | grep -w fms; then
-    echo " ==> Database already exists"
-else
-    echo " ==> Creating database"
+    echo " ==> Found `fms` database"
     psql -h db -U fms fms < /fms/db/schema.sql
     psql -h db -U fms fms < /fms/db/generate_secret.sql
     psql -h db -U fms fms < /fms/db/alert_types.sql
+else
+    echo " ==> Database `fms` not found"
 fi
