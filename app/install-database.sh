c88dc64f5bd48d6a8eb9f07a7664ff71f9682412
Dale-Kurt Murray <dalekurt.murray@gmail.com>
2015-08-04 10:00:08 -0400

    Fixed nginx config file. Updated Dockerfile

--- app/install-database.sh
+++ app/install-database.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
-if psql -lqt -h db -U fms | cut -d \| -f 1 | grep -w fms; then
-    echo " ==> Found `fms` database"
-    psql -h db -U fms fms < /fms/db/schema.sql
-    psql -h db -U fms fms < /fms/db/generate_secret.sql
-    psql -h db -U fms fms < /fms/db/alert_types.sql
-else
-    echo " ==> Database `fms` not found"
-fi
+#if psql -lqt | cut -d \| -f 1 | grep -w fms; then
+#if [[ `psql -h db -U fms -tAc "SELECT 1 FROM pg_database WHERE datname='fms'"` == "1" ]]; then
+#  echo " ==> Found database"
+#  if
+    psql -h db -U fms fms < /fixmystreet/db/schema.sql
+    psql -h db -U fms fms < /fixmystreet/db/generate_secret.sql
+    psql -h db -U fms fms < /fixmystreet/db/alert_types.sql
+#  else
+#    echo " Imporing database schema"
+#  fi
+#  else
+#    echo " ==> Database not found"
+#fi
