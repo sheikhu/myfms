0bed3221bbe2f2711157079bf6d44dfd326dc172
Dale-Kurt Murray <dalekurt.murray@gmail.com>
2015-08-11 10:57:32 -0400

    Clean up

--- app/install-database.sh
+++ app/install-database.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-#if psql -lqt | cut -d \| -f 1 | grep -w fms; then
-#if [[ `psql -h db -U fms -tAc "SELECT 1 FROM pg_database WHERE datname='fms'"` == "1" ]]; then
-#  echo " ==> Found database"
-#  if
     psql -h db -U fms fms < /fixmystreet/db/schema.sql
     psql -h db -U fms fms < /fixmystreet/db/generate_secret.sql
     psql -h db -U fms fms < /fixmystreet/db/alert_types.sql
-#  else
-#    echo " Imporing database schema"
-#  fi
-#  else
-#    echo " ==> Database not found"
-#fi
