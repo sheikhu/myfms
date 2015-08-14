8f776ea6da8f4c81e09c252058a580fe73c6db59
Dale-Kurt Murray <dalekurt.murray@gmail.com>
2015-08-14 10:40:38 -0400

    Fixed issues 1 and 2

--- app/install-database.sh
+++ app/install-database.sh
@@ -1,3 +1,7 @@
-    psql -h db -U fms fms < /fixmystreet/db/schema.sql
-    psql -h db -U fms fms < /fixmystreet/db/generate_secret.sql
-    psql -h db -U fms fms < /fixmystreet/db/alert_types.sql
+#! /bin/bash
+psql -h db -U fms fms < /fixmystreet/db/schema.sql
+psql -h db -U fms fms < /fixmystreet/db/generate_secret.sql
+psql -h db -U fms fms < /fixmystreet/db/alert_types.sql
+
+# reports
+/fixmystreet/bin/update-all-reports
