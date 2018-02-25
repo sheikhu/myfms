e7f4e670d10e653108bbc88a89db843a1b5b03a3
Sheikhu <sheikhu02@gmail.com>
2018-02-25 15:02:35 +0000

    Updates

--- app/install-database.sh
+++ app/install-database.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 psql -h db -U fms fms < /fixmystreet/db/schema.sql
 psql -h db -U fms fms < /fixmystreet/db/generate_secret.sql
-psql -h db -U fms fms < /fixmystreet/db/alert_types.sql
+psql -h db -U fms fms < /fixmystreet/db/fixture.sql
 
 # reports
 /fixmystreet/bin/update-all-reports
